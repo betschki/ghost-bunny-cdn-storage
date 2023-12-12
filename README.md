4ac6e529f868d70b39d91fd426848906462e16d8
Jannis Fedoruk-Betschki <github@betschki.com>
2023-12-12 16:12:28 +0100

    added support for environment variables

--- README.md
+++ README.md
@@ -12,8 +12,20 @@ This adapter implements the [Ghost documentation on storage adapters](https://gh
 3. Change into the directory with `cd adapters/storage`.
 4. Clone this git repository with `git clone https://github.com/betschki/ghost-bunny-cdn-storage.git bunny-cdn`.
 
-## Edit your configuration file
-In order for Ghost to use this storage adapter, you will need to edit your configuration file (e.g. `config.production.json`) and add the following:
+## Configuring the adapter
+To configure the adapter, you have two options. You can either edit your configuration file or set environment variables.
+
+Edit the individual values according to your BunnyCDN configuration. You can also have a look at the `configuration.example.json` or `example.env` files to see how the configuration should look like.
+
+The final URL with this sample configuration would look like this:
+
+```
+https://cdn.your-pull-zone-hostname.com/folder/file.jpg
+```
+
+
+### Configuration file
+If you want to set the configuration by editing your configuration file (e.g. `config.production.json`) add the following to it:
 
 ```json
 "storage":  {
@@ -28,13 +40,16 @@ In order for Ghost to use this storage adapter, you will need to edit your confi
 }
 ```
 
-Edit the values according to your BunnyCDN configuration. You can also have a look at the `configuration.sample.json` file in this repository for a sample configuration.
-
-The final URL with this sample configuration would look like this:
-
+### Environment variables
+To use environment variables, you need to set the following variables:
+```bash
+BUNNYCDN_ENDPOINT="https://storage.bunnycdn.com"
+BUNNYCDN_STORAGE_ZONE="your-storage-zone"
+BUNNYCDN_HOSTNAME="cdn.your-pull-zone-hostname.com"
+BUNNYCDN_FOLDER="a-folder-you-want-to-use"
+BUNNYCDN_ACCESS_KEY="your-access-key"
 ```
-https://cdn.your-pull-zone-hostname.com/folder/file.jpg
-```
+
 ### Setting a folder
 This adapter has been developed for the usage within [magicpages.co](https://magicpages.co) customer sites. One requirement was the ability to set a folder, as you can see in the final URL example above.
 
--- index.js
+++ index.js
@@ -5,164 +5,169 @@ const BaseAdapter = require('ghost-storage-base');
 const fs = require('fs');
 
 class BunnyCDNAdapter extends BaseAdapter {
-    constructor(config) {
-        super();
+  constructor(config) {
+    super();
 
-        this.options = config || {};
-        this.options.endpoint =
-            this.options.endpoint || 'https://storage.bunnycdn.com';
+    this.options = config || {};
+    this.options.endpoint =
+      this.options.endpoint ||
+      'https://storage.bunnycdn.com' ||
+      process.env.BUNNYCDN_ENDPOINT;
+    this.options.storageZone =
+      this.options.storageZone || process.env.BUNNYCDN_STORAGE_ZONE;
+    this.options.hostname =
+      this.options.hostname || process.env.BUNNYCDN_HOSTNAME;
+    this.options.accessKey =
+      this.options.accessKey || process.env.BUNNYCDN_ACCESS_KEY;
+    this.options.folder = this.options.folder || process.env.BUNNYCDN_FOLDER;
 
-        this.apiHeaders = {
-            AccessKey: this.options.accessKey
-        };
+    this.apiHeaders = {
+      AccessKey: this.options.accessKey,
+    };
+  }
+
+  /**
+   * Checks whether a file exists on BunnyCDN
+   * @param {string} filename
+   * @param {string} targetDir
+   * @returns {Promise<boolean>}
+   */
+  async exists(filename, targetDir) {
+    const files = await this.list(targetDir);
+    return files.includes(filename);
+  }
+
+  /**
+   * Saves a file to BunnyCDN
+   * @param {object} image - Image object with 'name' and 'path' properties
+   * @returns {Promise<string>} URL of the saved file
+   */
+  async save(image) {
+    if (!this.isValidImage(image)) {
+      throw new Error('Invalid image object. Image must have name and path.');
     }
 
-    /**
-     * Checks whether a file exists on BunnyCDN
-     * @param {string} filename
-     * @param {string} targetDir
-     * @returns {Promise<boolean>}
-     */
-    async exists(filename, targetDir) {
-        const files = await this.list(targetDir);
-        return files.includes(filename);
-    }
+    const uniqueFilename = this.generateUniqueFilename(image.name);
+    const uploadUrl = this.constructBunnyCDNUrl(uniqueFilename);
+    const fileStream = fs.createReadStream(image.path);
 
-    /**
-     * Saves a file to BunnyCDN
-     * @param {object} image - Image object with 'name' and 'path' properties
-     * @returns {Promise<string>} URL of the saved file
-     */
-    async save(image) {
-        if (!this.isValidImage(image)) {
-            throw new Error(
-                'Invalid image object. Image must have name and path.'
-            );
+    try {
+      await this.performUpload(uploadUrl, fileStream);
+      return this.constructDownloadUrl(uniqueFilename);
+    } catch (error) {
+      throw new Error(`Error during file save operation: ${error.message}`);
+    }
+  }
+
+  /**
+   * Ghost calls .serve() as part of its middleware stack,
+   * and mounts the returned function as the middleware for
+   * serving images
+   * @returns {function}
+   */
+  serve() {
+    return function customServe(req, res, next) {
+      next();
+    };
+  }
+
+  /**
+   * Deletes a file from BunnyCDN
+   * @param {string} filename
+   * @returns {Promise<void>}
+   */
+  async delete(filename) {
+    try {
+      await fetch(this.constructBunnyCDNUrl(filename), {
+        method: 'DELETE',
+        headers: this.apiHeaders,
+      });
+    } catch (error) {
+      throw new Error(`Error deleting file: ${error.message}`);
+    }
+  }
+
+  /**
+   * Reads a file from BunnyCDN
+   * @param {string} filename
+   * @returns {Promise<Buffer>}
+   */
+  async read(filename) {
+    try {
+      return fetch(this.constructBunnyCDNUrl(filename), {
+        method: 'GET',
+        headers: this.apiHeaders,
+      });
+    } catch (error) {
+      throw new Error(`Error reading file: ${error.message}`);
+    }
+  }
+
+  /**
+   * Returns a list of files in a directory
+   * @param {string} targetDir
+   * @returns {Promise<string[]>}
+   */
+  async list(targetDir) {
+    const folderPath = this.options.folder ? `/${this.options.folder}` : '';
+    try {
+      const response = await fetch(
+        `${this.options.endpoint}/${this.options.storageZone}${folderPath}/${targetDir}`,
+        {
+          method: 'GET',
+          headers: this.apiHeaders,
         }
+      );
 
-        const uniqueFilename = this.generateUniqueFilename(image.name);
-        const uploadUrl = this.constructBunnyCDNUrl(uniqueFilename);
-        const fileStream = fs.createReadStream(image.path);
+      if (!response.ok) {
+        throw new Error(`Error fetching files: ${response.statusText}`);
+      }
 
-        try {
-            await this.performUpload(uploadUrl, fileStream);
-            return this.constructDownloadUrl(uniqueFilename);
-        } catch (error) {
-            throw new Error(
-                `Error during file save operation: ${error.message}`
-            );
-        }
+      const json = await response.json();
+      return json;
+    } catch (error) {
+      throw new Error(`Error fetching files: ${error.message}`);
     }
+  }
 
-    /**
-     * Ghost calls .serve() as part of its middleware stack,
-     * and mounts the returned function as the middleware for
-     * serving images
-     * @returns {function}
-     */
-    serve() {
-        return function customServe(req, res, next) {
-            next();
-        };
+  isValidImage(image) {
+    return image && image.name && image.path;
+  }
+
+  generateUniqueFilename(originalName) {
+    return `${Date.now()}-${originalName}`;
+  }
+
+  constructBunnyCDNUrl(filename) {
+    const folderPath = this.options.folder ? `/${this.options.folder}` : '';
+    return `${this.options.endpoint}/${this.options.storageZone}${folderPath}/${filename}`;
+  }
+
+  constructDownloadUrl(filename) {
+    const folderPath = this.options.folder ? `/${this.options.folder}` : '';
+    return `https://${this.options.hostname}${folderPath}/${filename}`;
+  }
+
+  /**
+   * Performs the actual upload to BunnyCDN
+   * @param {string} url
+   * @param {fs.ReadStream} fileStream
+   * @returns {Promise<Response>}
+   */
+  async performUpload(url, fileStream) {
+    try {
+      return await fetch(url, {
+        method: 'PUT',
+        headers: {
+          ...this.apiHeaders,
+          'Content-Type': 'application/octet-stream',
+        },
+        body: fileStream,
+      });
+    } catch (error) {
+      throw new Error(`Error during upload: ${error.message}`);
     }
-
-    /**
-     * Deletes a file from BunnyCDN
-     * @param {string} filename
-     * @returns {Promise<void>}
-     */
-    async delete(filename) {
-        try {
-            await fetch(this.constructBunnyCDNUrl(filename), {
-                method: 'DELETE',
-                headers: this.apiHeaders
-            });
-        } catch (error) {
-            throw new Error(`Error deleting file: ${error.message}`);
-        }
-    }
-
-    /**
-     * Reads a file from BunnyCDN
-     * @param {string} filename
-     * @returns {Promise<Buffer>}
-     */
-    async read(filename) {
-        try {
-            return fetch(this.constructBunnyCDNUrl(filename), {
-                method: 'GET',
-                headers: this.apiHeaders
-            });
-        } catch (error) {
-            throw new Error(`Error reading file: ${error.message}`);
-        }
-    }
-
-    /**
-     * Returns a list of files in a directory
-     * @param {string} targetDir
-     * @returns {Promise<string[]>}
-     */
-    async list(targetDir) {
-        const folderPath = this.options.folder ? `/${this.options.folder}` : '';
-        try {
-            const response = await fetch(
-                `${this.options.endpoint}/${this.options.storageZone}${folderPath}/${targetDir}`,
-                {
-                    method: 'GET',
-                    headers: this.apiHeaders
-                }
-            );
-
-            if (!response.ok) {
-                throw new Error(`Error fetching files: ${response.statusText}`);
-            }
-
-            const json = await response.json();
-            return json;
-        } catch (error) {
-            throw new Error(`Error fetching files: ${error.message}`);
-        }
-    }
-
-    isValidImage(image) {
-        return image && image.name && image.path;
-    }
-
-    generateUniqueFilename(originalName) {
-        return `${Date.now()}-${originalName}`;
-    }
-
-    constructBunnyCDNUrl(filename) {
-        const folderPath = this.options.folder ? `/${this.options.folder}` : '';
-        return `${this.options.endpoint}/${this.options.storageZone}${folderPath}/${filename}`;
-    }
-
-    constructDownloadUrl(filename) {
-        const folderPath = this.options.folder ? `/${this.options.folder}` : '';
-        return `https://${this.options.hostname}${folderPath}/${filename}`;
-    }
-
-    /**
-     * Performs the actual upload to BunnyCDN
-     * @param {string} url
-     * @param {fs.ReadStream} fileStream
-     * @returns {Promise<Response>}
-     */
-    async performUpload(url, fileStream) {
-        try {
-            return await fetch(url, {
-                method: 'PUT',
-                headers: {
-                    ...this.apiHeaders,
-                    'Content-Type': 'application/octet-stream'
-                },
-                body: fileStream
-            });
-        } catch (error) {
-            throw new Error(`Error during upload: ${error.message}`);
-        }
-    }
+  }
 }
 
 module.exports = BunnyCDNAdapter;
